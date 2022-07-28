cd4cbcb881ceb45a3e46fa35d7dc9f6999ee2f64
Francesc Puig <f.puig.i.plana@accenture.com>
2022-07-28 02:59:52 +0200

    Update bot

---
+++ get_json.py
@@ -0,0 +1,47 @@
+import json
+from random import randint
+
+
+def get_messages(ppal, key):
+    f = open('reply_text.json')
+    data = json.load(f)
+    data = data[ppal]
+    val = data.pop(key, 'Ops! Messagio non trovato')
+    f.close()
+    return val
+
+
+def get_list_restaurants():
+    f = open('reply_text.json')
+    data = json.load(f)
+    data = data['restaurants']
+    f.close()
+    return data
+
+
+def get_random_restaurant():
+    f = open('reply_text.json')
+    data = json.load(f)
+    data = data['restaurants']
+    key = randint(1, len(data))
+    val = data.pop(str(key), 'Ops! Messagio non trovato')
+    f.close()
+    return val
+
+
+def update_json_new_restaurant(new_restaurant, username):
+    f = open('reply_text.json')
+    data = json.load(f)
+    key = str(len(data['restaurants']) + 1)
+    if "https://goo.gl/maps/" not in new_restaurant and 'maps.app.goo.gl' not in new_restaurant:
+        print(new_restaurant)
+        return "Ops! Link non valido, devi fornire un link con formato https://goo.gl/maps/<id>"
+    if 'Francesc' not in username and 'Maria' not in username and 'puxi' not in username and 'maria' not in username:
+        return "Ops! Non sei un utente autorizzato"
+    f.close()
+    data['restaurants'][key] = new_restaurant
+    print(data)
+    with open('reply_text.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+    return "Ok, ho capito, grazie per farmi imparare nuovi posti!"
---
+++ main.py
@@ -0,0 +1,62 @@
+from telegram.ext.updater import Updater
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.filters import Filters
+import os
+from get_json import get_messages, get_random_restaurant, update_json_new_restaurant, get_list_restaurants
+
+TOKEN = os.getenv('DOVESIMANGIA_TOKEN')
+updater = Updater(TOKEN, use_context=True)
+
+
+def start(update: Update, context: CallbackContext):
+    msg = get_messages('messages', 'start')
+    update.message.reply_text(msg)
+
+
+def get_restaurant(update: Update, context: CallbackContext):
+    print("New get random restaurant {} - {}".format(update, context))
+    update.message.reply_text(get_random_restaurant())
+
+
+def get_all_restaurants(update: Update, context: CallbackContext):
+    rests = get_list_restaurants()
+    for k, v in rests.items():
+        update.message.reply_text(v)
+
+
+def set_new_restaurant(update: Update, context: CallbackContext):
+    try:
+        ctx = update['message']
+        new_link = ctx['text'].replace('/nuovo ', '')
+        user = ctx['chat']['first_name']
+        print("User: {username} - Insert new link {link}".format(username=user, link=new_link))
+        resp = update_json_new_restaurant(new_restaurant=new_link, username=user)
+        update.message.reply_text(resp)
+    except Exception as err:
+        print(err)
+        update.message.reply_text('Ops! Qualcosa non è andata bene')
+
+
+def unknown(update: Update, context: CallbackContext):
+    update.message.reply_text("Ops! Mi dispiace, {0} non è un comando valido".format(update.message.text))
+
+
+def unknown_text(update: Update, context: CallbackContext):
+    update.message.reply_text("Ops! Mi dispiace non capisco cosa intendi: '%s'" % update.message.text)
+
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('random', get_restaurant))
+updater.dispatcher.add_handler(CommandHandler('lista', get_all_restaurants))
+updater.dispatcher.add_handler(CommandHandler('nuovo', set_new_restaurant))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
+updater.dispatcher.add_handler(MessageHandler(
+    Filters.command, unknown))  # Filters out unknown commands
+
+# Filters out unknown messages.
+updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
+
+updater.start_polling()
